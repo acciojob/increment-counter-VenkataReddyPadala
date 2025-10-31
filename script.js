2108d5cd3be48dce6c32b43fd8475573bed881fb
VenkataReddyPadala <84061954+VenkataReddyPadala@users.noreply.github.com>
2025-10-31 12:48:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const counter=document.querySelector('#counter');
+const btn = document.querySelector("#incrementBtn");
+let count=0;
+counter.textContent=count;
+btn.addEventListener("click",()=>{
+	count++;
+	counter.textContent=count;
+	alert(count);
+})
